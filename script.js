2f8a6267533fdd82b406c568a947a7e9f5e40d99
akuczera <97765892+akuczera@users.noreply.github.com>
2022-04-22 20:36:44 +0000

    Create loop to add 16 div squares

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 
 //Get access to <body> of HTML
 const body = document.body;
+const gridSize = 16;
 
+function buildGrid(x) {
 //Add etch-a-sketch container for all squares, set id attribute
 const sketchContainer = document.createElement("div");
-sketchContainer.setAttribute("id", 'container');
+sketchContainer.setAttribute("id", "container");
+console.log(sketchContainer.id);
 body.appendChild(sketchContainer);
+var sketchSquare;
 
-const sketchSquares = document.createElement("div");
+//This loop builds our grid
+  for (var i=0; i < x; i++) {
+    sketchSquare = document.createElement("div");
+    sketchSquare.className = "gridSquare";
+    sketchContainer.appendChild(sketchSquare);
+    console.log(sketchSquare.class);
+  }
+}
 
-
+buildGrid(gridSize);
