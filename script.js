2ac602452f0dd09cc3ff7655e823cbdb41891ee5
akuczera <97765892+akuczera@users.noreply.github.com>
2022-04-22 02:43:58 +0000

    Create container div.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+//Add etch-a-sketch container for all squares, set id attribute
+const sketchContainer = document.createElement("div");
+sketchContainer.setAttribute("id", 'container');
+
+
+const sketchSquares = document.createElement("div");
+
+
