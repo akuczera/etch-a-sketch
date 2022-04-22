31c0afb0547ee0e1b8915702d4ce21c175f346c7
akuczera <97765892+akuczera@users.noreply.github.com>
2022-04-22 02:56:11 +0000

    Append sketchContainer div to body of HTML

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
+//Get access to <body> of HTML
+const body = document.body;
+
 //Add etch-a-sketch container for all squares, set id attribute
 const sketchContainer = document.createElement("div");
 sketchContainer.setAttribute("id", 'container');
-
+body.appendChild(sketchContainer);
 
 const sketchSquares = document.createElement("div");
 
