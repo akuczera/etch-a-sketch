db56629a05417d84647e4fdeb6ec2d30182a8cf2
akuczera <97765892+akuczera@users.noreply.github.com>
2022-04-25 13:36:57 +0000

    Create function to listen for mouseover and change color of div

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # etch-a-sketch
+
+What I learned with this project:
+
+- To print an elements ID: console.log(element.id);
+- To print an elements class: console.log(element.className);
--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 
-//Get access to <body> of HTML
+//Get access to <body> of HTML.
 const body = document.body;
 const gridSize = 16;
 
 function buildGrid(x) {
-//Add etch-a-sketch container for all squares, set id attribute
+//Add etch-a-sketch container for all squares, set id attribute.
 const sketchContainer = document.createElement("div");
 sketchContainer.setAttribute("id", "container");
-  sketchContainer.className = "grid";
+sketchContainer.className = "grid";
 console.log(sketchContainer.id);
 body.appendChild(sketchContainer);
+
 var sketchSquare;
 
-//This loop builds our grid
+//This loop builds our grid.
   for (var i=0; i < x; i++) {
     sketchSquare = document.createElement("div");
     sketchSquare.className = "gridSquare";
+    console.log(sketchSquare.className);
     sketchContainer.appendChild(sketchSquare);
-    console.log(sketchSquare.class);
+    
   }
 }
 
 buildGrid(gridSize);
+
+//Hover effect that changes the color of each div when a use hovers over the div.
+const div = document.getElementsByClassName("gridSquare");
+
+div.forEach(div => div.addEventListener("mouseover", () => {
+  div.className = "changeColor";
+}));
