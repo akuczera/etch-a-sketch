243b51d647e48c108af62a98b3509c661f400589
akuczera <97765892+akuczera@users.noreply.github.com>
2022-04-27 03:33:09 +0000

    Created function to prompt user for gridsize of etch-a-sketch container

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 
 //Get access to <body> of HTML.
 const body = document.body;
-const gridSize = 16;
+var gridSize = 16;
 
 function buildGrid(x) {
 //Add etch-a-sketch container for all squares, set id attribute.
 const sketchContainer = document.createElement("div");
 sketchContainer.setAttribute("id", "container");
 sketchContainer.className = "grid";
-console.log(sketchContainer.id);
 body.appendChild(sketchContainer);
 
-var sketchSquare;
+let sketchSquare;
 
 //This loop builds our grid.
   for (var i=0; i < x; i++) {
     sketchSquare = document.createElement("div");
     sketchSquare.className = "gridSquare";
-    console.log(sketchSquare.className);
     sketchContainer.appendChild(sketchSquare);
   }
 }
@@ -25,19 +23,39 @@ var sketchSquare;
 buildGrid(gridSize);
 
 //Hover effect that changes the color of each div when a use hovers over the div.
-var div = document.querySelectorAll("div.gridSquare");
+let div = document.querySelectorAll("div.gridSquare");
 
 div.forEach(div => div.addEventListener("mouseover", () => {
   div.className = "changeColor";
 }));
 
-//Reset button to clear the colors in the gridSquares
+//Reset button to clear the colors in the gridSquares.
 const clearButton = document.querySelector("#clear");
 
 clearButton.addEventListener("click", () => {
-  console.log("clear!");
-  var clearDiv = document.querySelectorAll("div.changeColor");
+  let clearDiv = document.querySelectorAll("div.changeColor");
   clearDiv.forEach(element => {
     element.classList.toggle("changeColor");
     element.className = "gridSquare";
   })});
+
+//Grid size button that asks user how large to make the etch-a-sketch grid.
+let gridButton = document.querySelector("#gridSize");
+
+gridButton.addEventListener("click", () => {
+  console.log("Changing grid size!");
+  let userInput;
+  
+  while(true) {
+  userInput = parseInt(prompt("How many squares would you like? Enter 1 - 100"));
+ 
+  if (userInput < 1 || userInput > 100) {
+    userInput = parseInt(prompt("Please try again. Enter 1 - 100"));
+  } else {
+    console.log(userInput);
+    gridSize = userInput;
+    console.log(gridSize);
+    buildGrid(gridSize);
+    break;
+  }
+}});
