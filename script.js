c7df4386cd210e1ba2e53cbedcf98f33926e476e
akuczera <97765892+akuczera@users.noreply.github.com>
2022-04-27 03:38:43 +0000

    Removed code for sketchContainer from the function, hoisted it outside of the function, and added replaceChildren to function buildGrid to allow new grid size to be created.

--- script.js
+++ script.js
@@ -3,14 +3,15 @@
 const body = document.body;
 var gridSize = 16;
 
-function buildGrid(x) {
 //Add etch-a-sketch container for all squares, set id attribute.
 const sketchContainer = document.createElement("div");
 sketchContainer.setAttribute("id", "container");
 sketchContainer.className = "grid";
 body.appendChild(sketchContainer);
 
-let sketchSquare;
+function buildGrid(x) {
+sketchContainer.replaceChildren();
+var sketchSquare;
 
 //This loop builds our grid.
   for (var i=0; i < x; i++) {
